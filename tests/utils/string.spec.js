6f8a2fd7c3d2028a0793a179b3ccfb80c374d836
German Bilalov <pinncho@gmail.com>
2021-11-22 01:53:52 +0600

    use spyOn mocks to test utils/string module

--- tests/utils/string.spec.js
+++ tests/utils/string.spec.js
@@ -1,70 +1,70 @@
-import {
-    isUpperCase,
-    isLetter,
-    mirrorLetter,
-    shiftLetter,
-} from '../../src/utils/string.js';
+import { jest } from '@jest/globals';
+import * as string from '../../src/utils/string.js';
 
 describe('isUppercase() function', () => {
     test('should return true if passed char is uppercase', () => {
-        expect(isUpperCase('A')).toBeTruthy();
+        const isUpperCaseMock = jest.spyOn(string, 'isUpperCase');
+        const mockResult = isUpperCaseMock('A');
+        expect(mockResult).toBeTruthy();
     });
 
     test('should return false if passed char is not uppercase', () => {
-        expect(isUpperCase('a')).toBeFalsy();
+        const isUpperCaseMock = jest.spyOn(string, 'isUpperCase');
+        const mockResult = isUpperCaseMock('a');
+        expect(mockResult).toBeFalsy();
     });
 });
 
 describe('isLetter() function', () => {
     test('should return true if passed char is a letter', () => {
-        expect(isLetter('a')).toBeTruthy();
+        expect(string.isLetter('a')).toBeTruthy();
     });
 
     test('should return false if passed char is not a letter', () => {
-        expect(isLetter('1')).toBeFalsy();
+        expect(string.isLetter('1')).toBeFalsy();
     });
 });
 
 describe('mirrorLetter() function', () => {
     test('should mirror alphabet letter from A to Z', () => {
-        expect(mirrorLetter('A')).toBe('Z');
+        expect(string.mirrorLetter('A')).toBe('Z');
     });
 
     test('should mirror alphabet letter from Z to A', () => {
-        expect(mirrorLetter('Z')).toBe('A');
+        expect(string.mirrorLetter('Z')).toBe('A');
     });
 
     test('should return the same char if not letter', () => {
-        expect(mirrorLetter('$')).toBe('$');
+        expect(string.mirrorLetter('$')).toBe('$');
     });
 
     test('should return a mirrored lowercase letter when lowercase letter is being mirrored', () => {
-        expect(mirrorLetter('a', 1)).toBe('z');
+        expect(string.mirrorLetter('a', 1)).toBe('z');
     });
 });
 
 describe('shiftLetter() function', () => {
     test('should shift a letter by one position', () => {
-        expect(shiftLetter('A', 1)).toBe('B');
+        expect(string.shiftLetter('A', 1)).toBe('B');
     });
 
     test('should shift a letter by two positions', () => {
-        expect(shiftLetter('A', 2)).toBe('C');
+        expect(string.shiftLetter('A', 2)).toBe('C');
     });
 
     test('shifting the last letter of the alphabet by one should return "A"', () => {
-        expect(shiftLetter('Z', 1)).toBe('A');
+        expect(string.shiftLetter('Z', 1)).toBe('A');
     });
 
     test('should return a shifted lowercase letter when lowercase letter is being shifted', () => {
-        expect(shiftLetter('a', 1)).toBe('b');
+        expect(string.shiftLetter('a', 1)).toBe('b');
     });
 
     test('return the same char if not letter', () => {
-        expect(shiftLetter('$', 1)).toBe('$');
+        expect(string.shiftLetter('$', 1)).toBe('$');
     });
 
     test('should work with negative shifts', () => {
-        expect(shiftLetter('A', -1)).toBe('Z');
+        expect(string.shiftLetter('A', -1)).toBe('Z');
     });
 });
